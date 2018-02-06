ea81b304e03811a14174fa38f14e0d5b4f82e59f
Scott Chamberlain <myrmecocystus@gmail.com>
2018-02-06 07:18:21 -0800

    some tweaks to trigger gp feedback

--- R/foo_bar.R
+++ R/foo_bar.R
@@ -1,8 +1,12 @@
 #' foo_bar
 #' 
 #' @export
+#' @details asdfasdflajsdfals dflasj dfaljsdf asldfj asdlfja sdfljas dflja sdfljas dflajs dflajsdf alsjf aljsdf 
 #' @examples
 #' foo_bar()
 foo_bar <- function() {
+  x <- 5 + 5; if (x < 4 && x == 7 && x > 9 && x == x^2) x else x + 53345353345345353453534
+  z = 7
+  sapply(mtcars, class)
   return("foo bar")
 }
--- R/foobar3-package.R
+++ R/foobar3-package.R
@@ -1,4 +1,6 @@
 #' foobar3 package
+#' 
+#' asdlfjasdlfa fljas falsjfd dsalfj asljf asljf asljdf alfkj asldfj asldfj aslfj aslfjaslfasdf slj lj afs
 #'
 #' @name foobar3-package
 #' @aliases foobar3
